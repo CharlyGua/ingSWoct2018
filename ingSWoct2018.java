1e784bae1b15aa8979c2f36f76aeb114a77e7c0b
CharlyGua <43378277+CharlyGua@users.noreply.github.com>
2019-01-13 19:27:01 -0500

    Add files via upload

---
+++ ingSWoct2018.java
@@ -0,0 +1,19 @@
+
+package ingswoct2018;
+
+/**
+ *
+ * @author CarlosG  
+ */
+public class IngSWoct2018 {
+    //Este programa te muestra los números primos del 1 hasta el 100.
+
+    public static void main(String[] args) {
+        int x=1;
+        int y=100;
+        while(x<y){ y--;
+    System.out.println(y);
+    
+        }   
+    }                        
+}
